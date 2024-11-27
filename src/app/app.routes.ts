f91d202ad80775539c1ce27644f7e6084e64f9c7
SvetlozarStoyanov <svetlio214@gmail.com>
2024-11-27 16:46:05 +0200

    registered route to post details view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { homeResolver } from './core/resolvers/home.resolver';
+import { PostDetailsComponent } from './features/post-details/post-details.component';
 
 
 export const routes: Routes = [
-    { path: 'home', component: HomeComponent},
+    { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
-    { path: 'login', component: LoginComponent }
+    { path: 'login', component: LoginComponent },
+    { path: 'posts/details/:id', component: PostDetailsComponent }
 ];
