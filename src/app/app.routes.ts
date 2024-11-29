751ccace9c290e52dba6cf6123156cbb68351449
SvetlozarStoyanov <svetlio214@gmail.com>
2024-11-29 16:19:15 +0200

    Added not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './features/home/home.component';
 import { homeResolver } from './core/resolvers/home.resolver';
 import { PostDetailsComponent } from './features/post-details/post-details.component';
 import { SubforumDetailsComponent } from './features/subforum-details/subforum-details.component';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent },
     { path: 'login', component: LoginComponent },
     { path: 'posts/details/:id', component: PostDetailsComponent },
     { path: 'subforums/details/:name', component: SubforumDetailsComponent }
