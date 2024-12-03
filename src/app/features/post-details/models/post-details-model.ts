befa182e4bd9e0648be0d716500b4d7249fcf041
SvetlozarStoyanov <svetlio214@gmail.com>
2024-12-03 19:23:04 +0200

    Removed unnecessary properties

--- src/app/features/post-details/models/post-details-model.ts
+++ src/app/features/post-details/models/post-details-model.ts
@@ -1,11 +1,5 @@
 import { PostListModel } from "../../../core/models/post-list-model";
-import { SubforumMinInfoModel } from "../../../core/models/subforum-min-info-model";
-import { UserMinInfoModel } from "../../../core/models/user-min-info-model";
-import { CommentListModel } from "./comment-list-model";
 
 export interface PostDetailsModel {
-    post: PostListModel,
-    user: UserMinInfoModel,
-    subforum: SubforumMinInfoModel,
-    comments: CommentListModel[]
+    post: PostListModel
 }
